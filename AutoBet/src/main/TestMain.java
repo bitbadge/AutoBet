fc39eb462d25d03c623cdd71cbbb2dda0b9c53cc
aocon <a.oconnor124@gmail.com>
2016-04-24 13:28:25 -0400

    1.1

--- AutoBet/src/main/TestMain.java
+++ AutoBet/src/main/TestMain.java
@@ -1,17 +1,37 @@
 package main;
 
-import com.autobet.utility.MatchUtility;
+import java.io.File;
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
 
 public class TestMain {
-	public static void main(String[] args) {
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * MatchUtility.printAllMatchesToFile();
+	 * 
+	 * 
+	 * for (int i = 1; i < 7000; i++) {
+	 * System.out.println(MatchUtility.populateMatch("http://csgo.guru/match/" +
+	 * i)); }
+	 * 
+	 * 
+	 * }
+	 */
 
-		MatchUtility.printAllMatchesToFile();
+	/*public static void main(String[] args) throws Exception {
 
-		
-		/*for (int i = 1; i < 7000; i++) {
-			System.out.println(MatchUtility.populateMatch("http://csgo.guru/match/" + i));
-		}*/
-		
-		
-	}
+		// load CSV
+		CSVLoader loader = new CSVLoader();
+		loader.setSource(new File("match information compiled - cleaned match information.csv"));
+		Instances data = loader.getDataSet();
+
+		// save ARFF
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(data);
+		saver.setFile(new File("match information compiled - cleaned match information.arff"));
+		saver.writeBatch();
+	}*/
 }
